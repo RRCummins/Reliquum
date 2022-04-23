0beb477101e772193deeff608eb60dd51a9c09bf
Ryan Cummins <brokenrhino@gmail.com>
2022-04-22 22:44:13 -0400

    Added beginning layout

--- Reliquum/Reliquum/ContentView.swift
+++ Reliquum/Reliquum/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var entries: [Entry] = []
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button {
+                //
+            } label: {
+                Label("Add", image: "plus")
+            }
+            
+            List(entries) { entry in
+                HStack {
+                    Text(entry.name)
+                    
+                    Text("\(entry.date)")
+                }
+            }
+
+        }
     }
 }
 
