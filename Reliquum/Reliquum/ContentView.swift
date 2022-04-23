4a9fd38a7ca73fbc91b64d4f17fd230c2cdb5953
Ryan Cummins <brokenrhino@gmail.com>
2022-04-22 23:14:40 -0400

    Layout changes

--- Reliquum/Reliquum/ContentView.swift
+++ Reliquum/Reliquum/ContentView.swift
@@ -8,29 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var entries: [Entry] = []
+    var entries: [Entry] = []
+    @State private var colors:[Color] = [.indigo, .white]
     var body: some View {
         VStack {
-            Button {
-                //
-            } label: {
-                Label("Add", image: "plus")
+            HStack {
+                Spacer()
+                Button {
+                    //
+                } label: {
+                    Label("Add", systemImage: "plus")
+                        .font(.title)
+                        .labelStyle(.iconOnly)
+                        .padding()
+                        .background(colors[0])
+                        .foregroundColor(colors[1])
+                        .clipShape(Capsule())
+                        .padding(.horizontal)
+                }
             }
             
             List(entries) { entry in
                 HStack {
                     Text(entry.name)
+                        .font(.headline)
                     
-                    Text("\(entry.date)")
+                    Spacer()
+                    
+                    Text("\(entry.date.formatted(date: .abbreviated, time: .omitted))")
+                        .font(.subheadline)
+                    
+                    Text("\(entry.date.formatted(date: .omitted, time: .shortened))")
+                        .font(.subheadline.italic())
+                    
+                    Image(systemName: "\(entry.reminder ? "bell.fill" : "bell.slash.fill")")
+                        .foregroundColor(colors[0])
+                        .padding()
                 }
             }
-
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(entries: [Entry.example])
     }
 }
