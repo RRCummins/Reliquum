d57c2859c7cfd1b6f3de375ac7a9ddce183bc05c
Ryan Cummins <brokenrhino@gmail.com>
2022-04-22 22:27:30 -0400

    Reliquum xcode project init

---
+++ Reliquum/Reliquum/ReliquumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReliquumApp.swift
+//  Reliquum
+//
+//  Created by Ryan Cummins on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ReliquumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
